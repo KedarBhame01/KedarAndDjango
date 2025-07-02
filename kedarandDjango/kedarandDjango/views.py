2c6a7ba17e3f7a8e09df4a43db363ce7e6ba25c8
kedar <kedarbhame1999@gmail.com>
2025-07-02 13:07:22 +0530

    html page work with django

--- kedarandDjango/kedarandDjango/views.py
+++ kedarandDjango/kedarandDjango/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-# from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello guys my name is Kedar Bhame")
+    # return HttpResponse("Hello guys my name is Kedar Bhame")
+    return render(request,'website/index.html')
+
+def contact(request):
+    # return HttpResponse("You are in the contact page.")
+    return render(request,'website/contact.html')
