ee2ea211aee228b86ad8f264973dee0a4e090520
kedar <kedarbhame1999@gmail.com>
2025-07-08 15:20:54 +0530

    office

--- kedarandDjango/chai/models.py
+++ kedarandDjango/chai/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ChaiVariety(models.Model):
+    Chai_Type_Choice =[('ML','Masala'),('GR','Ginger'),('PL','Plain')]
+    name = models.CharField(max_length= 100)
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices = Chai_Type_Choice)
