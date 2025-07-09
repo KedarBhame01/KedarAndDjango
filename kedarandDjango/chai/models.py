5ae62a4f7762f5881f8199c7c2e5d40688c38c9b
KedarBhame01 <kedarbhame1999@gmail.com>
2025-07-09 08:24:06 +0530

    home

--- kedarandDjango/chai/models.py
+++ kedarandDjango/chai/models.py
@@ -6,3 +6,5 @@ class ChaiVariety(models.Model):
     name = models.CharField(max_length= 100)
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices = Chai_Type_Choice)
+    def __str__(self):
+        return self.name
