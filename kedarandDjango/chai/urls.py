5267421f24f55df321292864d9d26978a5908c96
KedarBhame01 <kedarbhame1999@gmail.com>
2025-07-10 07:17:11 +0530

    home

--- kedarandDjango/chai/urls.py
+++ kedarandDjango/chai/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-   path('',views.all_chai, name= 'all_chai')
+   path('',views.all_chai, name= 'all_chai'),
+   path('<int:chai_id>/',views.chai_detail, name='chai_detail')
 ]
